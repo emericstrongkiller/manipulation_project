02fe396056b9ba96d3ea94d50a039d43329adaef
emericstrongkiller <arhant.emeric-is@outlook.fr>
2025-07-06 13:13:50 +0000

    added and completed object_detection node (+associated custom msg)

---
+++ object_detection/launch/object_detection.launch.py
@@ -0,0 +1,32 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    rviz_config_dir = "/home/user/ros2_ws/src/manipulation_project/object_detection/config/rviz_perception_config.rviz"
+
+    return LaunchDescription([
+        Node(
+            package='object_detection',
+            executable='static_transform_publisher',
+            name='static_transform_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': True}]),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': True}],
+            output='screen'
+            ),
+        Node(
+            package='object_detection',
+            executable='object_detection',
+            name='object_detection',
+            output='screen',
+            parameters=[{'use_sim_time': True}])
+        ])
